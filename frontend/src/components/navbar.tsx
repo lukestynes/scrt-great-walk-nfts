8f3fff55e620c431ed046a2e58b047ce8f394b5a
Luke Stynes <lst89@uclive.ac.nz>
2024-10-12 22:24:20 +1300

    feat: update navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import Link from "next/link";
-import { CircleUser, Menu, Package2, Search } from "lucide-react";
+import {
+  CircleUser,
+  Menu,
+  MountainSnowIcon,
+  Package2,
+  Search,
+} from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -12,13 +18,28 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { ModeToggle } from "@/components/theme-toggle";
+import { usePathname } from "next/navigation";
 
-export const description =
-  "A settings page. The settings page has a sidebar navigation and a main content area. The main content area has a form to update the store name and a form to update the plugins directory. The sidebar navigation has links to general, security, integrations, support, organizations, and advanced settings.";
+export const navItems = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "Purchase",
+    href: "/purchase",
+  },
+  {
+    name: "Tickets",
+    href: "/tickets",
+  },
+];
 
 export function Navbar() {
+  const pathname = usePathname();
+
   const handleLogout = async () => {
     try {
       const response = await fetch("/api/auth/logout", {
@@ -27,7 +48,6 @@ export function Navbar() {
 
       if (response.ok) {
         console.log("Logged out successfully");
-        window.location.href = "/login"; // Redirect to the login page
       } else {
         console.error("Failed to log out");
       }
@@ -42,39 +62,21 @@ export function Navbar() {
           href="#"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
-          <Package2 className="h-6 w-6" />
-          <span className="sr-only">Acme Inc</span>
+          <MountainSnowIcon />
         </Link>
-        <Link
-          href="#"
-          className="text-muted-foreground transition-colors hover:text-foreground"
-        >
-          Dashboard
-        </Link>
-        <Link
-          href="#"
-          className="text-muted-foreground transition-colors hover:text-foreground"
-        >
-          Orders
-        </Link>
-        <Link
-          href="#"
-          className="text-muted-foreground transition-colors hover:text-foreground"
-        >
-          Products
-        </Link>
-        <Link
-          href="#"
-          className="text-muted-foreground transition-colors hover:text-foreground"
-        >
-          Customers
-        </Link>
-        <Link
-          href="#"
-          className="text-foreground transition-colors hover:text-foreground"
-        >
-          Settings
-        </Link>
+        {navItems.map((item) => (
+          <Link
+            key={item.name}
+            href={item.href ?? ""}
+            className={`hover:text-forground text-muted-foreground transition-colors ${
+              pathname === item.href
+                ? "text-primary"
+                : "text-muted-foreground hover:text-primary"
+            } `}
+          >
+            {item.name}
+          </Link>
+        ))}
       </nav>
       <Sheet>
         <SheetTrigger asChild>
@@ -122,17 +124,8 @@ export function Navbar() {
           </nav>
         </SheetContent>
       </Sheet>
-      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <form className="ml-auto flex-1 sm:flex-initial">
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search products..."
-              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-            />
-          </div>
-        </form>
+      <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <ModeToggle />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="secondary" size="icon" className="rounded-full">
