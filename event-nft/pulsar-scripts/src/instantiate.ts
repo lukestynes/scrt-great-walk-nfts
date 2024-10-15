c6a63018173c14d8122a7e0e52f0ee4fad2f1015
Luke Stynes <lst89@uclive.ac.nz>
2024-10-15 18:01:13 +1300

    feat: modify upload scripts for my contract

--- event-nft/pulsar-scripts/src/instantiate.ts
+++ event-nft/pulsar-scripts/src/instantiate.ts
@@ -2,8 +2,8 @@ import { SecretNetworkClient, Wallet } from "secretjs";
 import * as dotenv from "dotenv";
 import * as crypto from "node:crypto";
 
-dotenv.config();  // Load environment variables from .env file 
-const mnemonic = process.env.MNEMONIC;  // Retrieve the mnemonic
+dotenv.config(); // Load environment variables from .env file
+const mnemonic = process.env.MNEMONIC; // Retrieve the mnemonic
 
 const wallet = new Wallet(mnemonic);
 
@@ -15,50 +15,82 @@ const secretjs = new SecretNetworkClient({
   walletAddress: wallet.address,
 });
 
-const instantiateContract = async (codeId: string, contractCodeHash: string): Promise<string> => {    
-    const initMsg = {
-        name: "Critter NFT",
-        symbol: "CRITTER",
-        entropy: crypto.randomBytes(20).toString('hex'),
-        config: {
-            public_token_supply: true,
-        },
-    };
-    // const initMsg = {
-    //     admin: wallet.address
-    // };
-    let tx = await secretjs.tx.compute.instantiateContract(
-        {
-            code_id: codeId,
-            sender: wallet.address,
-            code_hash: contractCodeHash,
-            init_msg: initMsg,
-            label: "test contract" + Math.ceil(Math.random() * 10000000),
-        },
-        {
-            gasLimit: 400_000,
-        }
-    );
-    
-    //Find the contract_address in the logs
-    //@ts-ignore
-    const contractAddress = tx.arrayLog!.find((log) => log.type === "message" && log.key === "contract_address").value;
-    
-    return contractAddress;
+const instantiateContract = async (
+  codeId: string,
+  contractCodeHash: string
+): Promise<string> => {
+  // const initMsg = {
+  //     name: "Critter NFT",
+  //     symbol: "CRITTER",
+  //     entropy: crypto.randomBytes(20).toString('hex'),
+  //     config: {
+  //         public_token_supply: true,
+  //     },
+  // };
+
+  const initMsg = {
+    name: "Great Walk NFT",
+    symbol: "GWNFT",
+    walk_name: "Routeburn Track",
+    max_tickets: 50,
+    checkpoint_coords: [
+      "44.8221, 168.1136",
+      "44.7322, 168.1590",
+      "44.6593, 168.1694",
+    ],
+    checkpoint_names: ["Routeburn Shelter", "Falls Hut", "Lake Mackenzie"],
+    checkpoint_hints: [
+      "Start at Routeburn Shelter, a great place to begin your adventure.",
+      "Head towards the beautiful Falls Hut, nestled in the forest.",
+      "Make your way to Lake Mackenzie for a stunning view of the alpine lake.",
+    ],
+    badge_images: [
+      "routeburn_shelter_badge.svg",
+      "falls_hut_badge.svg",
+      "lake_mackenzie_badge.svg",
+    ],
+    entropy: "f8e1b2c1938d1c46b4f7079874",
+    config: {
+      public_token_supply: true,
+    },
+  };
+  // const initMsg = {
+  //     admin: wallet.address
+  // };
+  let tx = await secretjs.tx.compute.instantiateContract(
+    {
+      code_id: codeId,
+      sender: wallet.address,
+      code_hash: contractCodeHash,
+      init_msg: initMsg,
+      label: "test contract" + Math.ceil(Math.random() * 10000000),
+    },
+    {
+      gasLimit: 400_000,
+    }
+  );
+
+  //Find the contract_address in the logs
+  //@ts-ignore
+  const contractAddress = tx.arrayLog!.find(
+    (log) => log.type === "message" && log.key === "contract_address"
+  ).value;
+
+  return contractAddress;
 };
 
 export const main = async (): Promise<void> => {
-    if (process.argv.length !== 4) {
-        console.error('Expected two arguments!');
-        process.exit(1);
-    }
+  if (process.argv.length !== 4) {
+    console.error("Expected two arguments!");
+    process.exit(1);
+  }
 
-    let code_id = process.argv[2];
-    let code_hash = process.argv[3];
+  let code_id = process.argv[2];
+  let code_hash = process.argv[3];
 
-    const contract_address = await instantiateContract(code_id, code_hash);
-    
-    console.log("Contract address: ", contract_address);
-}
+  const contract_address = await instantiateContract(code_id, code_hash);
 
-main()
+  console.log("Contract address: ", contract_address);
+};
+
+main();
--- event-nft/pulsar-scripts/src/upload.ts
+++ event-nft/pulsar-scripts/src/upload.ts
@@ -2,8 +2,10 @@ import { SecretNetworkClient, Wallet } from "secretjs";
 import * as dotenv from "dotenv";
 import * as fs from "fs";
 
-dotenv.config();  // Load environment variables from .env file 
-const mnemonic = process.env.MNEMONIC;  // Retrieve the mnemonic
+dotenv.config(); // Load environment variables from .env file
+// const mnemonic = process.env.MNEMONIC;  // Retrieve the mnemonic
+const mnemonic =
+  "grab slot view laundry cotton airport base seven divert mix advice drill"; // Retrieve the mnemonic
 
 const wallet = new Wallet(mnemonic);
 
@@ -15,40 +17,52 @@ const secretjs = new SecretNetworkClient({
   walletAddress: wallet.address,
 });
 
+// const secretjs = new SecretNetworkClient({
+//   url: "http://209.38.23.8:1317/",
+//   chainId: "secretdev-1",
+//   wallet: wallet,
+//   walletAddress: wallet.address,
+// });
 
-const uploadContract = async (contract_wasm: Buffer): Promise<{code_id: string, code_hash?: string}> => {
-    let tx = await secretjs.tx.compute.storeCode(
-        {
-            sender: wallet.address,
-            wasm_byte_code: contract_wasm,
-            source: "",
-            builder: "",
-        },
-        {
-            gasLimit: 6_000_000,
-        }
-    );
+const uploadContract = async (
+  contract_wasm: Buffer
+): Promise<{ code_id: string; code_hash?: string }> => {
+  let tx = await secretjs.tx.compute.storeCode(
+    {
+      sender: wallet.address,
+      wasm_byte_code: contract_wasm,
+      source: "",
+      builder: "",
+    },
+    {
+      gasLimit: 6_000_000,
+    }
+  );
 
-    //@ts-ignore
-    const codeId = tx.arrayLog!.find((log) => log.type === "message" && log.key === "code_id").value;
-  
-    console.log("codeId: ", codeId);
-  
-    const contractCodeHash = (
-        await secretjs.query.compute.codeHashByCodeId({ code_id: codeId })
-    ).code_hash;
-    console.log(`Contract hash: ${contractCodeHash}`);
-    return {
-        code_id: codeId,
-        code_hash: contractCodeHash,
-    };
+  //@ts-ignore
+  const codeId = tx.arrayLog!.find(
+    (log) => log.type === "message" && log.key === "code_id"
+  ).value;
+
+  console.log("codeId: ", codeId);
+
+  const contractCodeHash = (
+    await secretjs.query.compute.codeHashByCodeId({ code_id: codeId })
+  ).code_hash;
+  console.log(`Contract hash: ${contractCodeHash}`);
+  return {
+    code_id: codeId,
+    code_hash: contractCodeHash,
+  };
 };
 
 export const main = async (): Promise<void> => {
-    const { code_id, code_hash } = await uploadContract(fs.readFileSync("../contract.wasm.gz"));
-    
-    console.log("Code ID: ", code_id);
-    console.log("Code hash: ", code_hash);
-}
+  const { code_id, code_hash } = await uploadContract(
+    fs.readFileSync("../contract.wasm.gz")
+  );
 
-main()
+  console.log("Code ID: ", code_id);
+  console.log("Code hash: ", code_hash);
+};
+
+main();
